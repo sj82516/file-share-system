548536c40dc14256ed004d152a3ad589175ea75a
yuanchieh-at <yuanchieh@amazingtalker.com>
2023-03-05 10:50:15 +0800

    fix: redis connection

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,16 +19,10 @@ Rails.application.configure do
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-  if Rails.root.join("tmp/caching-dev.txt").exist?
-    config.cache_store = :memory_store
-    config.public_file_server.headers = {
-      "Cache-Control" => "public, max-age=#{2.days.to_i}"
-    }
-  else
-    config.action_controller.perform_caching = false
-
-    config.cache_store = :null_store
-  end
+  config.cache_store = :redis_cache_store, {
+    url: "redis://#{ENV['REDIS_SERVICE']}:6379/0",
+    namespace: 'my_app_cache'
+  }
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
