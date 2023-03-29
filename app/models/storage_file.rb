ca2917390efe82b0b515e22adb54220719d650b7
sj82516 <sj82516@gmail.com>
2023-03-29 08:32:07 +0800

    refactor: update syntax

--- app/models/storage_file.rb
+++ app/models/storage_file.rb
@@ -21,6 +21,8 @@ class StorageFile < ApplicationRecord
   enum status: { init: 0, uploaded: 1 }
 
   def shared?
-    shared_at.present? && shared_expired_at.present? && shared_expired_at > Time.now
+    return false unless shared_at.present?
+    return false unless shared_expired_at.present? && shared_expired_at > Time.now
+    true
   end
 end
